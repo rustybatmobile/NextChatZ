2b9bd670beec4300d7954a83d04baeb70d212279
Harish Neel <contact@harishcode.com>
2023-05-26 22:47:20 +0530

    classes add for the logo

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,19 @@
+'use client'
+
 import { FC }  from 'react';
 
 interface pageProps {}
 
 const page: FC<pageProps> = ({}) => {
-    return <div>page</div>
+    return <>
+        <div className = "flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+            <div className='w-full flex flex-col items-center max-w-md space-y-8'>
+                <div className='flex flex-col items-center gap-8'>
+                    logo
+                </div>
+            </div>
+        </div>
+    </>
 }
 
 export default page;
