d75ab4a74e9f00d9cc5ea6414c3e0dfaab5d4c51
Harish Neel <contact@harishcode.com>
2023-05-26 22:52:39 +0530

    Login with Google button styled

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,16 @@
 'use client'
 
-import { FC }  from 'react';
+import Button from '@/components/ui/Button';
+import { FC, useState }  from 'react';
 
 interface pageProps {}
 
 const page: FC<pageProps> = ({}) => {
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+
+    async function loginWithGoogle() {
+
+    }
     return <>
         <div className = "flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
             <div className='w-full flex flex-col items-center max-w-md space-y-8'>
@@ -12,6 +18,11 @@ const page: FC<pageProps> = ({}) => {
                     logo
                     <h2 className='mt-6 text-center text-3xl font-bold tracking-tight text-grey-900'>Sign in to your account</h2>
                 </div>
+                <Button
+                    isLoading = {isLoading}
+                    className = "max-w-sm mx-auto w-full"
+                    onClick={loginWithGoogle}
+                ></Button>
             </div>
         </div>
     </>
